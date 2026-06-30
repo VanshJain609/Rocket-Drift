a4ef53fbf672ec7e9aab5a7196a79daed157029c
VanshJain609 <jainvansh178@gmail.com>
2026-06-30 19:25:09 +0530

    Adding delay in loading the level

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,43 +4,59 @@ using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
 {
+    [SerializeField] private float delayTime = 2.0f;
     private void OnCollisionEnter(UnityEngine.Collision other)
     {
         // To make the working of objects by using tags 
-        switch(other.gameObject.tag)
+        switch (other.gameObject.tag)
         {
-            case "Friendly" :
+            case "Friendly":
                 Debug.Log("You have hit friendly");
                 break;
-            
-            case "Finish" :
+
+            case "Finish":
                 Debug.Log("You have hit finish");
-                LoadNextLevel();
+                StartLevelSequence();
                 break;
-            
+
             default:
                 Debug.Log("You don't hit anything");
-                RestartLevel();
+                CrashLevelSequence();
                 break;
-            
+
         }
+    }
 
-        void LoadNextLevel()
-        {
-            int currentScene = SceneManager.GetActiveScene().buildIndex;
-            int nextScene = currentScene + 1;
+    void StartLevelSequence()
+    {
+        // To add delay in loading the next level
+        Invoke("LoadNextLevel", delayTime);
+    }
 
+    void CrashLevelSequence()
+    {
+        // To add delay in restarting the level
+        Invoke("RestartLevel",  delayTime);
+    }
+    
+    void LoadNextLevel()
+    {
+        // To load the next level
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+
+        // if all the levels are completed then restart the first level 
             if (nextScene == SceneManager.sceneCountInBuildSettings)
             {
-                nextScene = 0;
+                    nextScene = 0;
             }
-            SceneManager.LoadScene(nextScene);
-        }
-        
-        void RestartLevel()
-        {
-            int currentScene = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentScene);
-        }
+        SceneManager.LoadScene(nextScene);
+    }
+            
+    void RestartLevel()
+    {
+        // To restart the level
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
     }
 }
