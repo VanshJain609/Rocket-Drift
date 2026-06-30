fbfe0de19050c62fa1cf451080c071662b7d4409
VanshJain609 <jainvansh178@gmail.com>
2026-06-30 18:00:15 +0530

    CollisionAdding

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -1,6 +1,24 @@
+
 using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    
+    private void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Friendly" :
+                Debug.Log("You have hit friendly");
+                break;
+            
+            case "Finish" :
+                Debug.Log("You have hit finish");
+                break;
+            
+            default:
+                Debug.Log("You don't hit anything");
+                break;
+            
+        }
+    }
 }
